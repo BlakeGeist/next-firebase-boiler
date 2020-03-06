ec8df10531370789689f3b02780ce9402b3a2725
blakegeist <rightfoot@gmail.com>
2020-03-06 15:09:22 -0800

    test styled components
    test styled components

--- src/app/pages/_app.js
+++ src/app/pages/_app.js
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import getStrings from '../helpers/getStrings'
 import setUserState from '../helpers/setUserState'
 import redirectIfNoLanguage from '../helpers/redirectIfNoLanguage'
+import { ServerStyleSheet } from 'styled-components';
 
-const MyApp = ({ Component, pageProps, reduxStore }) => {
+const MyApp = ({ Component, pageProps, reduxStore, styleTags }) => {
   return (
     <Provider store={reduxStore}>
       <Component {...pageProps} />
+      {styleTags}
     </Provider>
   );
 };
@@ -21,7 +23,15 @@ MyApp.getInitialProps = async ({ Component, ctx }) => {
   await getStrings(ctx)
   ctx.reduxStore.dispatch({ type: "SET_ITEM", name: "lang", payload: ctx.query.lang });    
   const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
-  return { pageProps };
+
+  const sheet = new ServerStyleSheet();
+
+
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+
+
+  return { pageProps, styleTags };
 };
 
 export default withReduxStore(MyApp);
