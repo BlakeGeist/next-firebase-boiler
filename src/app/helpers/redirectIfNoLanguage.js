1e755340ed805603654e17da5a7ad6ec0fa8d6aa
blakegeist <rightfoot@gmail.com>
2020-03-06 23:17:07 -0800

    try with return
    try with return

--- src/app/helpers/redirectIfNoLanguage.js
+++ src/app/helpers/redirectIfNoLanguage.js
@@ -3,8 +3,6 @@ export default (ctx) => {
     const userRegionLang = headers["accept-language"].split(",")[0].split("-");
     const userLang = userRegionLang[0];
     const userRegion = userRegionLang[1].toLowerCase();
-    console.log('this happened 2')
-
     if (ctx.res && ctx.asPath === "/") {
         console.log('this happened')
         ctx.res.writeHead(301, {
@@ -12,6 +10,6 @@ export default (ctx) => {
             // Add the content-type for SEO considerations
             "Content-Type": "text/html; charset=utf-8",
         })
-        ctx.res.end();
+        return ctx.res.end();
     }
 };
