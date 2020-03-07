9d9f816edd9e529de001943bf591603c2c7d5ab5
blakegeist <rightfoot@gmail.com>
2020-03-06 22:56:58 -0800

    test a though
    test a though

--- src/app/helpers/redirectIfNoLanguage.js
+++ src/app/helpers/redirectIfNoLanguage.js
@@ -2,13 +2,13 @@ export default async (ctx) => {
     const headers = ctx.req.headers;
     const userRegionLang = headers["accept-language"].split(",")[0].split("-");
     const userLang = userRegionLang[0];
-    const userRegion = userRegionLang[1].toLowerCase();    
+    const userRegion = userRegionLang[1].toLowerCase();
     if (ctx.res && ctx.asPath === "/") {
         ctx.res.writeHead(301, {
             Location: `/${userLang}`,
             // Add the content-type for SEO considerations
             "Content-Type": "text/html; charset=utf-8",
-        });
+        })
         ctx.res.end();
     }
 };
