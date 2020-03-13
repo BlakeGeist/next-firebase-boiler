05e57043310a29c8fab8b3f66f9d038622705e43
blakegeist <rightfoot@gmail.com>
2020-03-13 11:51:17 -0700

    move and update tinymce
    move and update tinymce

--- src/app/layouts/Layout.js
+++ src/app/layouts/Layout.js
@@ -20,36 +20,36 @@ const Layout = ({ children, pageMod }) => {
             <LoadingSpinner />
             <Modal />
             <style global jsx>{`
-          html, body, #__next {
-            height: 100%;
-          }
-          #__next{
-            display: flex;
-            flex-direction: column;
-          }
-          body{
-            background-color: #f1f1f1;
-          }
-          main {
-            flex: 1 1 100%;
-          }
-          main .container {
-            height: 100%;
-            background-color: #fff;
-            min-height: 500px;
-          }
-          .container {
-            max-width: 1150px;
-            margin: 0 auto;
-          }
-          .container .container {
-            padding: 0 24px;
-          }
-          h1 {
-            margin: 0;
-            padding: 2rem 0 1rem;            
-          }
-      `}</style>
+              html, body, #__next {
+                height: 100%;
+              }
+              #__next{
+                display: flex;
+                flex-direction: column;
+              }
+              body{
+                background-color: #f1f1f1;
+              }
+              main {
+                flex: 1 1 100%;
+              }
+              main .container {
+                height: 100%;
+                background-color: #fff;
+                min-height: 500px;
+              }
+              .container {
+                max-width: 1150px;
+                margin: 0 auto;
+              }
+              .container .container {
+                padding: 0 24px;
+              }
+              h1 {
+                margin: 0;
+                padding: 2rem 0 1rem;            
+              }
+          `}</style>
         </>
     );
 };
