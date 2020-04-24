eb8f8e9be4f30851f6546b6c88251510cc4a7968
blakegeist <rightfoot@gmail.com>
2020-04-24 12:39:26 -0700

    refactor calls

--- src/app/pages/helpers/firebaseAdmin.js
+++ src/app/pages/helpers/firebaseAdmin.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const Admin = () => {
     if (!admin.apps.length) {
         admin.initializeApp({
-            credential: admin.credential.cert(require("../../../credentials/server"))
+            credential: admin.credential.cert(require("../../credentials/server"))
         });
     };    
     return admin
